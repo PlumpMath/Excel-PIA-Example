29107e6b0af61b438e3d7280cdbea7dce5e022bd
Justin Dearing <zippy1981@gmail.com>
2012-06-27 06:27:40 -0400

    Initial version of the code.
    Visual Studio 2008 and 2010 solutions.
    Using log4net via nuget.
    Comments to explain how to leak memory.

---
+++ JustAProgrammer.PIA.Excel/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using log4net;
+using MSExcel = Microsoft.Office.Interop.Excel;
+
+
+namespace JustAProgrammer.PIA.Excel
+{
+    class Program
+    {
+        private static ILog log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args) {
+
+            var xlApp = new MSExcel.ApplicationClass();
+            var xlWorkbooks = xlApp.Workbooks;
+            xlApp.Visible = false;
+            xlApp.DisplayAlerts = false;
+
+            for (int i = 0; i < 500; i++)
+            {
+
+                var workbook = Path.Combine(Environment.CurrentDirectory, "us_foreign_assistance.xls");
+                //var workbook = "https://explore.data.gov/download/5gah-bvex/XLS";
+                var xlWorkBook = xlWorkbooks._Open(workbook, Missing.Value, Missing.Value, Missing.Value,
+                                                       Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                                                       Missing.Value, Missing.Value, Missing.Value, Missing.Value,
+                                                       Missing.Value);
+                
+
+                var xlWorkSheet = (MSExcel._Worksheet)xlWorkBook.Sheets["Notes"];
+                xlWorkSheet.Delete();
+
+                xlApp.Visible = false;
+                workbook = Path.Combine(Environment.CurrentDirectory, string.Format("Book-{0}.xls", i));
+                xlWorkBook.SaveAs(workbook, MSExcel.XlFileFormat.xlWorkbookNormal, Missing.Value, Missing.Value,
+                                  false, false, MSExcel.XlSaveAsAccessMode.xlNoChange, Missing.Value, Missing.Value,
+                                  Missing.Value, Missing.Value, Missing.Value);
+                // Commenting this out will cause memory usage to spike.
+                xlWorkBook.Close(false, Missing.Value, false);
+                log.InfoFormat("Wrote \"{0}\".", workbook);
+                // Commenting this out does not seem to affect memory usage.
+                Marshal.ReleaseComObject(xlWorkBook);
+            }
+            xlApp.DisplayAlerts = true;
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkbooks);
+            Marshal.ReleaseComObject(xlApp);
+
+            /*
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(false);
+            Console.WriteLine();
+             */
+        }
+    }
+}
